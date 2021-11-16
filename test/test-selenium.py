daad6ad681f5ec63db5f2423141a9136d375a4e5
althoenm <althoenm@umich.edu>
2021-11-15 19:29:12 -0600

    fill our individual service record fields with dummy data

--- test/test-selenium.py
+++ test/test-selenium.py
@@ -1,7 +1,7 @@
 # %%
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from secrets import username, password
+from secrets import username, password, test_record
 import getpass
 import time
 
@@ -38,6 +38,52 @@ except:
 # %%
 # Find the login button and click it
 submit = driver.find_element_by_xpath('/html/body/form/center/div/table/tbody/tr[3]/td/input').click()
-time.sleep(5)
+time.sleep(2)
 
-driver.close()
+# %%
+# Find the Individual Services button and click it
+
+i_path = driver.find_element_by_xpath('//*[@id="side_2"]/a').click()
+time.sleep(2)
+# %%
+# Find 'add new individual service record' button and click it
+add_new_path = driver.find_element_by_xpath('//*[@id="content_div"]/div[2]/table/tbody/tr[4]/td/a').click()
+# %%
+# Identify the fields and enter the data
+contact_date = driver.find_element_by_xpath('//*[@id="in_118clone"]').send_keys(test_record['contact_date'])
+time.sleep(3)
+
+# %%
+time_begun = driver.find_element_by_xpath('//*[@id="in_528"]').send_keys(test_record['time_begun'])
+time.sleep(.25)
+
+# %%
+time_ended = driver.find_element_by_xpath('//*[@id="in_529"]').send_keys(test_record['time_ended'])
+time.sleep(.25)
+
+# %%
+hours = driver.find_element_by_xpath('//*[@id="in_123"]').send_keys(test_record['hours'])
+time.sleep(.25)
+# %%
+consumer_field = driver.find_element_by_xpath('//*[@id="in_119"]')
+time.sleep(1.5)
+
+# %%
+individual_path = driver.find_element_by_xpath(f"//*[contains(text(), '{test_record['consumer']}')]").click()
+time.sleep(.25)
+
+# %%
+service_path = driver.find_element_by_xpath('//*[@id="in_120"]').send_keys(test_record['service'])
+time.sleep(.25)
+# %%
+priority_area_path = driver.find_element_by_xpath('//*[@id="in_340"]').send_keys(test_record['priority_area'])
+time.sleep(.25)
+# %%
+funding_source = driver.find_element_by_xpath('//*[@id="in_127"]').send_keys(test_record['funding_source'])
+time.sleep(.25)
+# %% 
+staff_comments = driver.find_element_by_xpath('//*[@id="in_132"]').send_keys(test_record['staff_coments'])
+time.sleep(.25)
+
+# %%
+ir = driver.find_element_by_xpath('//*[@id="in_135"]').send_keys(test_record['ir'])
