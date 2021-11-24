317da13d603fe9eedbaa484d736df50345e9dd71
Matteo Althoen <althoenm@umich.edu>
2021-11-24 14:03:28 -0600

    logic to enter records in subsequent input file rows

--- test/test-comm-activities.py
+++ test/test-comm-activities.py
@@ -67,16 +67,11 @@ submit = driver.find_element_by_xpath('/html/body/form/center/div/table/tbody/tr
 time.sleep(2)
 
 # %%
-# Find the Community Activities button and click it
-i_path = driver.find_element_by_xpath('//*[@id="side_4"]/a').click()
-time.sleep(2)
-# %%
-# Find 'add new community activities record' button and click it
-add_new_path = driver.find_element_by_xpath('//*[@id="content_div"]/div[2]/table/tbody/tr[4]/td/a').click()
+
 
 # %%
 # remove '0' from 09:00, 07:00 etc --> 9:00, 7:00
-
+# Convert datetime.time formats
 def fix_time(time):
     t = time
     t = str(t.strftime("%H:%M"))
@@ -90,52 +85,105 @@ def fix_date(date):
     d = str(d.strftime('%m/%d/%Y'))
     return d
 
-for data in datas:
-    date = fix_date(data[0])
-    hours = data[1]
-    time_begun = fix_time(data[2])
-    time_ended = fix_time(data[3])
-    issue_area = data[4].strip()
-    projects = data[5].strip()
-    service_program = data[6].strip()
-    funding_source = data[7]
-    note = data[8]
 
-# %% 
-    # Enter date
-    contact_date = driver.find_element_by_xpath('//*[@id="in_244clone"]').send_keys(date)
-    time.sleep(1.5) 
+for i in range(len(datas)):
+    date = fix_date(datas[i][0])
+    hours = datas[i][1]
+    time_begun = fix_time(datas[i][2])
+    time_ended = fix_time(datas[i][3])
+    issue_area = datas[i][4].strip()
+    projects = datas[i][5].strip()
+    service_program = datas[i][6].strip()
+    funding_source = datas[i][7]
+    note = datas[i][8]
 
-    # Enter time begun
-    time_begun = driver.find_element_by_xpath('//*[@id="in_530"]').send_keys(time_begun)
-    time.sleep(.25)
+    if i == 0:
+        # Find the Community Activities button and click it
+        i_path = driver.find_element_by_xpath('//*[@id="side_4"]/a').click()
+        time.sleep(2)
+        
+        # Find 'add new community activities record' button and click it
+        add_new_path = driver.find_element_by_xpath('//*[@id="content_div"]/div[2]/table/tbody/tr[4]/td/a').click()
 
-    # Time ended HH:MM
-    time_ended = driver.find_element_by_xpath('//*[@id="in_531"]').send_keys(time_ended)
-    time.sleep(.25)
+        # Enter date
+        contact_date = driver.find_element_by_xpath('//*[@id="in_244clone"]').send_keys(date)
+        time.sleep(1.5) 
 
-    # Hours (float)
-    hours = driver.find_element_by_xpath('//*[@id="in_250"]').send_keys(hours)
-    time.sleep(.25)
+        # Enter time begun
+        time_begun = driver.find_element_by_xpath('//*[@id="in_530"]').send_keys(time_begun)
+        time.sleep(.25)
 
-    # Consumer Field (last name, first name)
-    issue_area = driver.find_element_by_xpath('//*[@id="in_247"]').send_keys(issue_area)
-    time.sleep(.25)
+        # Time ended HH:MM
+        time_ended = driver.find_element_by_xpath('//*[@id="in_531"]').send_keys(time_ended)
+        time.sleep(.25)
 
-    # Enter service type dropdown 
-    projects = driver.find_element_by_xpath('//*[@id="in_258"]').send_keys(projects)
-    time.sleep(.25)
+        # Hours (float)
+        hours = driver.find_element_by_xpath('//*[@id="in_250"]').send_keys(hours)
+        time.sleep(.25)
 
-    # Enter priority area dropdown
-    service_program = driver.find_element_by_xpath('//*[@id="in_248"]').send_keys(service_program)
-    time.sleep(.25)
+        # Issue area dropdown 
+        issue_area = driver.find_element_by_xpath('//*[@id="in_247"]').send_keys(issue_area)
+        time.sleep(.25)
 
-    # Enter funding source dropdown
-    funding_source = driver.find_element_by_xpath('//*[@id="in_303"]').send_keys(funding_source)
-    time.sleep(.25)
+        # Projects dropdown
+        projects = driver.find_element_by_xpath('//*[@id="in_258"]').send_keys(projects)
+        time.sleep(.25)
 
-    # Enter social worker's note in text box
-    staff_comments = driver.find_element_by_xpath('//*[@id="in_255"]').send_keys(note)
-    time.sleep(.25)
+        # service program dropdown
+        service_program = driver.find_element_by_xpath('//*[@id="in_248"]').send_keys(service_program)
+        time.sleep(.25)
 
-# %%
+        # Enter funding source dropdown
+        funding_source = driver.find_element_by_xpath('//*[@id="in_303"]').send_keys(funding_source)
+        time.sleep(.25)
+
+        # Enter social worker's note in text box
+        staff_comments = driver.find_element_by_xpath('//*[@id="in_255"]').send_keys(note)
+        time.sleep(.25)
+
+        # Click save record
+        save_record = driver.find_element_by_xpath('//*[@id="sub"]').click()
+
+    else:
+        # find add a new record (from 'record successfully saved') and click it
+        add_new_path = driver.find_element_by_xpath('//*[@id="data_form"]/table[1]/tbody/tr[4]/td/input[3]').click()
+        time.sleep(0.25)
+
+        # Enter date
+        contact_date = driver.find_element_by_xpath('//*[@id="in_244clone"]').send_keys(date)
+        time.sleep(1.5) 
+
+        # Enter time begun
+        time_begun = driver.find_element_by_xpath('//*[@id="in_530"]').send_keys(time_begun)
+        time.sleep(.25)
+
+        # Time ended HH:MM
+        time_ended = driver.find_element_by_xpath('//*[@id="in_531"]').send_keys(time_ended)
+        time.sleep(.25)
+
+        # Hours (float)
+        hours = driver.find_element_by_xpath('//*[@id="in_250"]').send_keys(hours)
+        time.sleep(.25)
+
+        # Issue area dropdown 
+        issue_area = driver.find_element_by_xpath('//*[@id="in_247"]').send_keys(issue_area)
+        time.sleep(.25)
+
+        # Projects dropdown
+        projects = driver.find_element_by_xpath('//*[@id="in_258"]').send_keys(projects)
+        time.sleep(.25)
+
+        # service program dropdown
+        service_program = driver.find_element_by_xpath('//*[@id="in_248"]').send_keys(service_program)
+        time.sleep(.25)
+
+        # Enter funding source dropdown
+        funding_source = driver.find_element_by_xpath('//*[@id="in_303"]').send_keys(funding_source)
+        time.sleep(.25)
+
+        # Enter social worker's note in text box
+        staff_comments = driver.find_element_by_xpath('//*[@id="in_255"]').send_keys(note)
+        time.sleep(.25)
+
+        # Click save record
+        save_record = driver.find_element_by_xpath('//*[@id="sub"]').click()
--- test/test-individual-service.py
+++ test/test-individual-service.py
@@ -29,7 +29,6 @@ datas = []
 for i in range(1, len(sheet_cells)):
     datas.append(sheet_cells[i])
 
-print(datas)
 # %%
 # Open Chrome
 driver = webdriver.Chrome('/Users/matte/Downloads/chromedriver')
@@ -64,75 +63,149 @@ except:
 submit = driver.find_element_by_xpath('/html/body/form/center/div/table/tbody/tr[3]/td/input').click()
 time.sleep(2)
 
-# %%
-# Find the Individual Services button and click it
+# find the individual service button and click it
 i_path = driver.find_element_by_xpath('//*[@id="side_2"]/a').click()
-time.sleep(2)
-# %%
-# Find 'add new individual service record' button and click it
+time.sleep(1)
+
+# find add a new record and click it 
 add_new_path = driver.find_element_by_xpath('//*[@id="content_div"]/div[2]/table/tbody/tr[4]/td/a').click()
+time.sleep(0.25)
 
 # %%
 # remove '0' from 09:00, 07:00 etc --> 9:00, 7:00
 def fix_time(time):
-    if time[0] != '1':
-        return time[1:]
+    t = time
+    t = t.strftime("%H:%M")
+    str(t)
+    if t[0] != '1':
+        return t[1:]
     else:
-        return time
+        return t
+    
+def fix_date(date):
+    d = date
+    d = d.strftime('%m/%d/%Y')
+    d = str(d)
+    return d
 
-for data in datas:
-    name = data[0]
-    date = data[1]
-    hours = data[2]
-    time_begun = fix_time(data[3])
-    time_ended = fix_time(data[4])
-    service = data[5].strip()
-    priority_area = data[6].strip()
-    funding = data[7].strip()
-    staff_comment = data[8]
-    ir = data[9]
 
-    # Enter date
-    contact_date = driver.find_element_by_xpath('//*[@id="in_118clone"]').send_keys(date)
-    time.sleep(1.5) 
+# %%
 
-    # Enter time begun
-    time_begun = driver.find_element_by_xpath('//*[@id="in_528"]').send_keys(time_begun)
-    time.sleep(.25)
+for i in range(len(datas)):
+    name = datas[i][0]
+    date = fix_date(datas[i][1])
+    hours = datas[i][2]
+    time_begun = fix_time(datas[i][3])
+    time_ended = fix_time(datas[i][4])
+    service = datas[i][5].strip()
+    priority_area = datas[i][6].strip()
+    funding = datas[i][7].strip()
+    staff_comment = datas[i][8]
+    ir = datas[i][9]
 
-    # Time ended HH:MM
-    time_ended = driver.find_element_by_xpath('//*[@id="in_529"]').send_keys(time_ended)
-    time.sleep(.25)
+    # if len(driver.find_elements_by_xpath(f"//*[contains(text(), '{name}')]")) < 1:
+    #     print(f"{name} not found")
+    #     i += 1
 
-    # Hours (float)
-    hours = driver.find_element_by_xpath('//*[@id="in_123"]').send_keys(hours)
-    time.sleep(.25)
+    if i == 0:
 
-    # Consumer Field (last name, first name)
+        # Enter date
+        contact_date = driver.find_element_by_xpath('//*[@id="in_118clone"]').send_keys(date)
+        time.sleep(1.5) 
 
-    # Click dropdown, allow time to load elements
-    consumer_field = driver.find_element_by_xpath('//*[@id="in_119"]')
-    time.sleep(1.5)
+        # Enter time begun
+        time_begun = driver.find_element_by_xpath('//*[@id="in_528"]').send_keys(time_begun)
+        time.sleep(.25)
 
-    # Search elements in dropdown by consumer name, click on the first one
-    individual_path = driver.find_element_by_xpath(f"//*[contains(text(), '{name}')]").click()
-    time.sleep(.25)
+        # Time ended HH:MM
+        time_ended = driver.find_element_by_xpath('//*[@id="in_529"]').send_keys(time_ended)
+        time.sleep(.25)
 
-    # Enter service type dropdown 
-    service_path = driver.find_element_by_xpath('//*[@id="in_120"]').send_keys(service)
-    time.sleep(.25)
+        # Hours (float)
+        hours = driver.find_element_by_xpath('//*[@id="in_123"]').send_keys(hours)
+        time.sleep(.25)
 
-    # Enter priority area dropdown
-    priority_area_path = driver.find_element_by_xpath('//*[@id="in_340"]').send_keys(priority_area)
-    time.sleep(.25)
+        # Consumer Field (last name, first name)
 
-    # Enter funding source dropdown
-    funding_source = driver.find_element_by_xpath('//*[@id="in_127"]').send_keys(funding)
-    time.sleep(.25)
+        # Click dropdown, allow time to load elements
+        consumer_field = driver.find_element_by_xpath('//*[@id="in_119"]')
+        time.sleep(1.5)
 
-    # Enter social worker's note in text box
-    staff_comments = driver.find_element_by_xpath('//*[@id="in_132"]').send_keys(staff_comment)
-    time.sleep(.25)
+        # Search elements in dropdown by consumer name, click on the first one
+        individual_path = driver.find_element_by_xpath(f"//*[contains(text(), '{name}')]").click()
+        time.sleep(.25)
 
-    # Enter whether this note is an I&R (binary dropdown)
-    ir = driver.find_element_by_xpath('//*[@id="in_135"]').send_keys(ir)
+        # Enter service type dropdown 
+        service_path = driver.find_element_by_xpath('//*[@id="in_120"]').send_keys(service)
+        time.sleep(.25)
+
+        # Enter priority area dropdown
+        priority_area_path = driver.find_element_by_xpath('//*[@id="in_340"]').send_keys(priority_area)
+        time.sleep(.25)
+
+        # Enter funding source dropdown
+        funding_source = driver.find_element_by_xpath('//*[@id="in_127"]').send_keys(funding)
+        time.sleep(.25)
+
+        # Enter social worker's note in text box
+        staff_comments = driver.find_element_by_xpath('//*[@id="in_132"]').send_keys(staff_comment)
+        time.sleep(.25)
+
+        # Enter whether this note is an I&R (binary dropdown)
+        ir = driver.find_element_by_xpath('//*[@id="in_135"]').send_keys(ir)
+
+        # Click save record
+        save_record = driver.find_element_by_xpath('//*[@id="sub"]').click()
+
+    else:
+        # find add a new record (from 'record successfully saved') and click it
+        add_new_path = driver.find_element_by_xpath('//*[@id="data_form"]/table[1]/tbody/tr[4]/td/input[3]').click()
+        time.sleep(0.25)
+
+        # Enter date
+        contact_date = driver.find_element_by_xpath('//*[@id="in_118clone"]').send_keys(date)
+        time.sleep(1.5) 
+
+        # Enter time begun
+        time_begun = driver.find_element_by_xpath('//*[@id="in_528"]').send_keys(time_begun)
+        time.sleep(.25)
+
+        # Time ended HH:MM
+        time_ended = driver.find_element_by_xpath('//*[@id="in_529"]').send_keys(time_ended)
+        time.sleep(.25)
+
+        # Hours (float)
+        hours = driver.find_element_by_xpath('//*[@id="in_123"]').send_keys(hours)
+        time.sleep(.25)
+
+        # Consumer Field (last name, first name)
+
+        # Click dropdown, allow time to load elements
+        consumer_field = driver.find_element_by_xpath('//*[@id="in_119"]')
+        time.sleep(1.5)
+
+        # Search elements in dropdown by consumer name, click on the first one
+        individual_path = driver.find_element_by_xpath(f"//*[contains(text(), '{name}')]").click()
+        time.sleep(.25)
+
+        # Enter service type dropdown 
+        service_path = driver.find_element_by_xpath('//*[@id="in_120"]').send_keys(service)
+        time.sleep(.25)
+
+        # Enter priority area dropdown
+        priority_area_path = driver.find_element_by_xpath('//*[@id="in_340"]').send_keys(priority_area)
+        time.sleep(.25)
+
+        # Enter funding source dropdown
+        funding_source = driver.find_element_by_xpath('//*[@id="in_127"]').send_keys(funding)
+        time.sleep(.25)
+
+        # Enter social worker's note in text box
+        staff_comments = driver.find_element_by_xpath('//*[@id="in_132"]').send_keys(staff_comment)
+        time.sleep(.25)
+
+        # Enter whether this note is an I&R (binary dropdown)
+        ir = driver.find_element_by_xpath('//*[@id="in_135"]').send_keys(ir)
+
+        # Click save record
+        save_record = driver.find_element_by_xpath('//*[@id="sub"]').click()
